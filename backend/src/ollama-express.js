7806c4c9b9ebf0c37365dcbb6956daf5821a2651
matt51814 <matthawthorn@hotmail.com>
2024-10-25 20:57:03 +0100

    chore(javascript): clean up code

--- backend/src/ollama-express.js
+++ backend/src/ollama-express.js
@@ -13,48 +13,42 @@ const ollama = new Ollama({ host: 'http://host.docker.internal:11434' });
 
 const modelfile = `
 FROM llama3.2:1b
-SYSTEM "Create all your responses in html format with a header summarising your response using a h2 tag."
+SYSTEM "Create all your responses in html format with a header summarising your response using a h2 tag. Keep your resposes concise."
 `;
 await ollama.create({ model: 'my_model', modelfile: modelfile });
-
-
-
 
 const messages_list = [];
 
 router.get('/', async (req, res) => {
     try {
-        res.status(200).send('Ollama Express app running')
+        res.status(200).send('Ollama Express app running');
     } catch (error) {
-        res.status(500).send('Error processing your request: ' + error.message);
+        res.status(500).send('Error processing your request: ' + error.message);;
     }
 });
 
 router.post('/ask-query', async (req, res) => {
     const { query } = await req.body;
     console.log(query);
-    var content = `{"role": "user","content": "${query}"}`;
+    //query = query.trim(); // remove whitespace as this breaks everything
+    //var content = `{"role": "user","content": "${query}"}`;
+    var content = JSON.stringify({
+        role: "user",
+        content: query.trim()
+    });
     messages_list.push(JSON.parse(content));
     console.log(messages_list);
     try {
         const output = await ollama.chat({
-            model: 'my_model',//'llama3.2:1b',
+            model: 'my_model',
             messages: messages_list
-            // messages: [{
-            //     "role": "user",
-            //     "content": query
-            // }]
         });
         messages_list.push(output.message);
         res.send(output);
-        //console.log(output.message);
-        // message_history.push(JSON.parse(output["message"]));
-
-        //messages: [{ role: 'user', content: 'Why is the sky blue?' }]
     } catch (error) {
         res.status(500).send('Error processing your request: ' + error.message);
     }
-})
+});
 
 app.use('/api', router);
 
