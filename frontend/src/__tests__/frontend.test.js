73c43b279df45faa74bb7bbbd39b985415da77a0
matt51814 <matthawthorn@hotmail.com>
2024-11-08 21:42:01 +0000

    tests: began testing async code

--- frontend/src/__tests__/frontend.test.js
+++ frontend/src/__tests__/frontend.test.js
@@ -2,7 +2,8 @@
  * @jest-environment jsdom
  */
 
-import { userTextBoxHtml, llmTextBoxHtml, createLlmTextBox } from '../ollama-frontend.js';
+import { userTextBoxHtml, llmTextBoxHtml, createLlmTextBox, fetchPostRequest } from '../ollama-frontend.js';
+import 'cross-fetch/polyfill';
 
 test('generates correct user HTML element', () => {
     expect(userTextBoxHtml(1, "test")).toBe(`
@@ -30,3 +31,68 @@ test('test creating LLM text box', () => {
     </div>
     <br>`);
 });
+
+
+
+
+// ASYNC TESTS
+test('test fetchPostRequest', () => {
+    const queryUrl = "http://host.docker.internal:3000/api/ask-query";
+    var requestOptions = {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            "query": "return True"
+        }),
+    };
+
+
+    async function testFetchPostRequest(url, options) {
+        const result = await fetch(url, options)
+        .then(res => res.body)
+        .then(rb => rb.on('readable', () => {
+            let chunk;
+            while(null !== (chunk = rb.read())) {
+                console.log(chunk.toString());
+            }
+        }))
+        return result
+    }
+
+
+    return testFetchPostRequest(queryUrl, requestOptions).then(data => {
+        expect(data).toBe('my_model')
+    })
+});
+
+
+// export async function fetchPostRequest(url, options) {
+//     const result = await fetch(url, options)
+//     .then(res => res.body)
+//     .then(rb => {
+//         const reader = rb.getReader()
+//         return new ReadableStream({
+//             start(controller) {
+//                 function push() {
+//                     reader.read().then(async ({done, value}) => {
+//                         if (done) {
+//                             controller.close()
+//                             return
+//                         }
+//                         // Fetch the individual words
+//                         controller.enqueue(value)
+//                         let json = JSON.parse(new TextDecoder().decode(value))
+//                         push()
+//                     })
+//                 }
+//                 push()
+//             }
+//         })
+//     })
+//     .then(stream => 
+//         new Response(stream, { headers: { "Content-Type": "text/html" } }).json()
+//     )
+//     return result;
+// };
